73e4000ede8eb70ba7860fb7edef5f87fcfa40d7
mjrt0817 <tanemura@accompany-partners.jp>
2026-07-02 08:29:05 +0900

    Add files via upload

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,19 +3,18 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAfgEV9oDQQPnlP7r-RnAYRKlEB6vL-S5I",
-  authDomain: "gen-lang-client-0125274260.firebaseapp.com",
-  projectId: "gen-lang-client-0125274260",
-  storageBucket: "gen-lang-client-0125274260.firebasestorage.app",
-  messagingSenderId: "525064586221",
-  appId: "1:525064586221:web:a118584b93f9db49669de4"
+  apiKey: "AIzaSyD_jxDqcmvArllrXrt9-Rs5eZpP-Iv1yDs",
+  authDomain: "nen-bedb6.firebaseapp.com",
+  projectId: "nen-bedb6",
+  storageBucket: "nen-bedb6.firebasestorage.app",
+  messagingSenderId: "677674566791",
+  appId: "1:677674566791:web:6b587898ae4b778a41a422"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
 
-// Custom databaseId is required for this sandboxed project database
-const db = getFirestore(app, "ai-studio-3-ca5e0052-3eb7-4eb7-92c9-d3a7b526068c");
+const db = getFirestore(app);
 
 export { auth, googleProvider, db, signInWithPopup, signOut };
